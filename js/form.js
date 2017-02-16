cb34ce3763dd5d7b1639fc05585fead76d647916
Юлия <ivanova.js.7@gmail.com>
2017-02-16 19:15:32 +0500

    Оформление контента

---
+++ js/form.js
@@ -0,0 +1,45 @@
+var btn = document.querySelector(".js-open-form");
+var popup = document.querySelector(".modal-content");
+var close = popup.querySelector(".modal-content-close");
+var form = popup.querySelector(".write-form");
+var username = popup.querySelector("[name=username]");
+var email = popup.querySelector("[name=email]");
+var storage = localStorage.getItem("username");
+
+btn.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.add("modal-content-show");
+  if (storage) {
+    username.value = storage;
+    email.focus();
+  } else {
+    username.focus();
+  }    
+});
+
+close.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.remove("modal-content-show");
+  popup.classList.remove("modal-error")
+});
+
+form.addEventListener("submit", function(event) {
+  if (!username.value || !email.value) {
+    event.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } else {
+    localStorage.setItem("username", username.value);
+  }
+});
+
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (popup.classList.contains("modal-content-show")) {
+      popup.classList.remove("modal-content-show");
+      popup.classList.remove("modal-error")
+    }
+  }
+});
